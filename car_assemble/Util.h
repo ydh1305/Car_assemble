97a83f92ec24ee2a38d8c79b6a965f483e46555e
donghyun.yoon <ydh1305@naver.com>
2026-06-11 10:15:54 +0900

    [fix][util]: remove obsolete part-type enums causing SUV name conflict
    SUV enumerator (value=2) conflicted with class SUV in the same namespace,
    causing C2365 compile error in Phase3_CarValidatorTests.cpp.
    These enums are unused after OCP refactoring in Phase 3.

--- car_assemble/Util.h
+++ car_assemble/Util.h
@@ -1,10 +1,6 @@
 #pragma once
 
 enum QuestionType { CarType_Q, Engine_Q, brakeSystem_Q, SteeringSystem_Q, Run_Test };
-enum CarType { SEDAN=1, SUV, TRUCK };
-enum Engine { GM=1, TOYOTA, WIA };
-enum brakeSystem { MANDO=1, CONTINENTAL, BOSCH_B };
-enum SteeringSystem { BOSCH_S=1, MOBIS };
 enum ValidationResult { VALID, FAIL_SEDAN_CONTINENTAL, FAIL_SUV_TOYOTA, FAIL_TRUCK_WIA, FAIL_TRUCK_MANDO, FAIL_BOSCH_MISMATCH };
 
 static const int VALID_RANGE[][2] = { {1,3},{0,4},{0,3},{0,2},{0,2} };
